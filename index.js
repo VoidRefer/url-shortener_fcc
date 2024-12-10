a06013b7e3e0af1fc7fa36e4da0a6fc7a52fcf45
Alex Soler <alexsolerbaca14@gmail.com>
2024-12-10 16:41:46 +0000

    feat: add url shortener

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const bodyParser = require('body-parser');
+const { status } = require('express/lib/response');
 const app = express();
 
 // Basic Configuration
@@ -18,6 +21,73 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Database
+let urlDatabase = [];
+let idCounter = 1;
+
+// Helper function for validating urls
+function validateUrl(url, callback) {
+  try {
+    const urlObject = new URL(url);
+    dns.lookup(urlObject.hostname, (err) => {
+      if (err) console.log(err);
+      callback(true);
+    });
+  } catch {
+    callback(false);
+  }
+}
+
+// POST /api/shorturl
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+
+  // Validate url
+  validateUrl(originalUrl, (isValid) => {
+    if (!isValid) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    // Check if url is in the database
+    let existingEntry = urlDatabase.find(entry => entry.original_url === originalUrl);
+    if (existingEntry) {
+      return res.json({
+        original_url: originalUrl,
+        short_url: existingEntry.short_url
+      })
+    }
+
+    // Add new entry to database
+    const shortUrl = idCounter++;
+    urlDatabase.push({
+      original_url: originalUrl,
+      short_url: shortUrl
+    });
+    res.json({
+      original_url: originalUrl,
+      short_url: shortUrl
+    });
+  })
+})
+
+// GET /api/shorturl/<short_url>
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const shortUrl = parseInt(req.params.short_url);
+
+  // Find the original url
+  const entry = urlDatabase.find(entry => entry.short_url === shortUrl);
+  if (!entry){
+    return res.status(404).json({ error: "An error occurred"});
+  }
+
+  // Redirerect to original url
+  res.redirect(entry.original_url);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
